8bb4358a0e1db6f8066f1d3a00a2617e942baf8e
luyaneluiz <luyane.luiz.ll@gmail.com>
2023-10-04 16:05:17 -0300

    criando e estilizando página not found

--- src/pages/notfound/index.tsx
+++ src/pages/notfound/index.tsx
@@ -1,3 +1,13 @@
+import { Link } from 'react-router-dom';
+import styles from './notfound.module.css'
+
 export function NotFound() {
-  return <div></div>;
+  return(
+    <div className={styles.container}>
+      <h1>Página 404 não existe</h1>
+      <Link to="/">
+        Acessar cripto moedas
+      </Link>
+    </div>
+  );
 }
